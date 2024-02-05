159bc1584b4569841224e11afaebfb91e8d632da
peacepiece7 <peacepiece7@naver.com>
2024-02-06 07:54:59 +0900

    async component, provide/inject

---
+++ playground/src/components/B3_async_child_component.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>B3_async_child_component</div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped>
+  section { margin: 40px; }
+</style>
---
+++ playground/src/components/B3_provide_inject_child.vue
@@ -0,0 +1,19 @@
+<template>
+  <section>
+    <div>B3_provide_inject_child</div>
+    <slot name="title">
+      <h1>Default Title</h1>
+    </slot>
+  </section>
+  <section>
+    <B3_provide_inject_deep_child />
+  </section>
+</template>
+
+<script setup>
+import B3_provide_inject_deep_child from './B3_provide_inject_deep_child.vue';
+</script>
+
+<style scoped>
+  section { margin: 40px; }
+</style>
